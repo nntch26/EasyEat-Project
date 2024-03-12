8d1ee1be4dbfacf63008acfef26c6fd27e34766a
Tumacsp <tumacsp@gmail.com>
2024-03-12 14:20:21 +0700

    show reserv info

--- Cashier_history.php
+++ Cashier_history.php
@@ -1,25 +1,29 @@
 <?php
 
-
+include('backEnd/includes/connectDB.php');
+$get_payment_info = $db->prepare("SELECT * FROM Payments");
+$get_payment_info->execute();
+$payment_info = $get_payment_info->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <h2>ประวัติการขาย</h2>
 <table class="tablemain">
     <tr>
         <th>ID</th>
-        <th>สถานะ</th>
+        <th>Bill ID</th>
         <th>วันที่ทำรายการ</th>
+        <th>เวลาที่ทำรายการ</th>
         <th>ราคารวม</th>
     </tr>
-    <tr>
-        <td>1150</td>
-        <td>ยังไม่ตาย</td>
-        <td>วัน ทู ทรี โฟ</td>
-        <td>เป็นล้านเลยพี่</td>
-    </tr>
-    <tr>
-        <td>1150</td>
-        <td>ยังไม่ตาย</td>
-        <td>วัน ทู ทรี โฟ</td>
-        <td>เป็นล้านเลยพี่</td>
-    </tr>
+    <?php
+    foreach ($payment_info as $row) {
+        echo '<tr>';
+        echo '<td>'. $row['payment_id'] .'</td>';
+        echo '<td>'. $row['Bill_id'] . '</td>';
+        echo '<td>'. $row['payment_date'] .'</td>';
+        echo '<td>'. $row['payment_time'] .'</td>';
+        echo '<td>'. $row['payment_total'] .'</td>';
+        echo '</tr>';
+    }
+    ?>
 </table>
